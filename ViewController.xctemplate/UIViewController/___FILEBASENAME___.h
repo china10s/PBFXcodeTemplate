bfad1488c49961b7956f134d7572c5fd71f11bbf
guest <guest@zhulindeMacBook-Pro.local>
2016-07-07 16:22:14 +0800

    Initial Commit

---
+++ ViewController.xctemplate/UIViewController/___FILEBASENAME___.h
@@ -0,0 +1,17 @@
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+___IMPORTHEADER_cocoaSubclass___
+
+@interface ___FILEBASENAME___: ___VARIABLE_cocoaSubclass___
+#pragma mark- as
+
+#pragma mark- model
+
+#pragma mark- view
+
+#pragma mark- api
+
+@end
---
+++ ViewController.xctemplate/UIViewController/___FILEBASENAME___.m
@@ -0,0 +1,38 @@
+//
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAME___ ()
+
+@end
+
+@implementation ___FILEBASENAME___
+
+#pragma mark - def
+
+#pragma mark - override
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - api
+
+#pragma mark - model event
+#pragma mark 1 notification
+#pragma mark 2 KVO
+
+#pragma mark - view event
+#pragma mark 1 target-action
+#pragma mark 2 delegate dataSource protocol
+
+#pragma mark - private
+#pragma mark - getter / setter
+
+#pragma mark -
+
+@end
